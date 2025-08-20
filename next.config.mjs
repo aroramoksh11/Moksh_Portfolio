ee233a2a7406c800ca579d065c63f6be3d0f39e0
Moksh Arora <moksh.arora@isikko.com>
2025-08-20 15:46:32 +0530

    fix: migrate next.config.js to next.config.mjs for ES module compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-require("./src/env.js");
+import "./src/env.js";
 import WithPWA from "next-pwa";
 
 const withPWA = WithPWA({
